391faf923e1843210171f4c3d30ec1aebd678f25
Yash Chopra <theyash920@gmail.com>
2025-09-05 16:12:21 +0530

    removed techstack section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Testimonials from "../sections/Testimonials";
 import Footer from "../sections/Footer";
 import Contact from "../sections/Contact";
-import TechStack from "../sections/TechStack";
 import Solutions from "../sections/Solutions";
 import Hero from "../sections/Hero";
 import ShowcaseSection from "../sections/ShowcaseSection";
@@ -13,7 +12,6 @@ const Home = () => (
     <ShowcaseSection />
     <LogoShowcase />
     <Solutions />
-    <TechStack />
     <Testimonials />
     <Contact />
   </>
